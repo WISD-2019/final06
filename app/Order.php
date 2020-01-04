9ca36c8be577918fc8929085e4d6a9f0d49e6535
Sue Qing Ya <3a632009@student.ncut.edu.tw>
2020-01-04 17:28:00 +0800

    建立關聯

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function Ticket(){
+        return $this->hasMany(Ticket ::class);
+    }
+    public function To_Order(){
+        return $this->belongsTo(User::class,'member_id');
+    }
 }
