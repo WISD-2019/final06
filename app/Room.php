892eee1a92979ff0864ecbf3ee74317c11204887
Sue Qing Ya <3a632009@student.ncut.edu.tw>
2020-01-04 19:10:37 +0800

    建立關聯

--- app/Room.php
+++ app/Room.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
-    //
+    public function Currently_Showing(){
+        return $this->hasOne(Movie::class);
+    }
+    public function Scheduled_Show(){
+        return $this->hasMany(Movie::class);
+    }
 }
--- app/Section.php
+++ app/Section.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Section extends Model
 {
-    //
+    public function Ticket(){
+        return $this->hasMany(Ticket::class);
+    }
+    public function Room(){
+        return $this->belongsTo(Room::class);
+    }
 }
