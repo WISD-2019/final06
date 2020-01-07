7172915b3aac0f02799b07b2ed7a66f45f1c7ccf
3A632016 <winnie771737@gmail.com>
2020-01-08 02:25:12 +0800

    錯誤修正

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -1,14 +1,11 @@
 @foreach ($movie as $movie)
     <body bgcolor="#E6E6FA">
     <div style="font-family: '微軟正黑體' ">
-    <p> <h3 font-color="#000000">電影 :{{ $movie->name}}</h3></p>
-    <p>類型:{{ $movie->category}}片</p>
-    <p>{{ $movie->pic}}</p>
-    <p>簡介 : {{ $movie->introduction}}</p>
-    <p>片長 : {{ $movie->length}}</p>
-    <hr size="8px" align="center" width="100%">
+        <p> <h3 font-color="#000000">電影 :{{ $movie->name}}</h3></p>
+        <p>類型:{{ $movie->category}}片</p>
+        <p>{{ $movie->pic}}</p>
+        <p>簡介 : {{ $movie->introduction}}</p>
+        <p>片長 : {{ $movie->length}}</p>
+        <hr size="8px" align="center" width="100%">
     </div>
 @endforeach
-
-
-
